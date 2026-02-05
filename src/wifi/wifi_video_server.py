67e3bc0f62836271025fbb22257fcbdc99da7c53
Seongho Cho <rtfgcv0214@kaist.ac.kr>
2026-02-05 16:52:51 +0900

    [feat] add video save option

--- src/wifi/wifi_video_server.py
+++ src/wifi/wifi_video_server.py
@@ -1,36 +1,87 @@
 import cv2
 import signal
 import sys
+import argparse
 
-PORT = 8001
+parser = argparse.ArgumentParser(description="WiFi Video Server using FFmpeg")
+parser.add_argument("-p", "--port", type=int, default=8001, help="Port to listen on for incoming video stream")
+parser.add_argument("-d", "--display", action="store_true", help="Display the video stream in a window")
+parser.add_argument("-f", "--save_file", type=str, default="", help="File path to save the incoming video stream")
 
-cap = cv2.VideoCapture(
-    f"udp://0.0.0.0:{PORT}?overrun_nonfatal=1&fifo_size=50000000",
-    cv2.CAP_FFMPEG
-)
 
-if not cap.isOpened():
-    print("Failed to open video stream")
-    sys.exit(1)
+cap: cv2.VideoCapture | None = None
+writer: cv2.VideoWriter | None = None
+
 
 def cleanup(signum=None, frame=None):
+    global cap, writer
+
     print("Cleaning up...")
-    cap.release()
+
+    if cap is not None:
+        cap.release()
+
+    if writer is not None:
+        writer.release()
+
     cv2.destroyAllWindows()
     sys.exit(0)
 
-signal.signal(signal.SIGINT, cleanup)
-signal.signal(signal.SIGTERM, cleanup)
+def main():
+    global cap, writer
+    args = parser.parse_args()
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        continue
+    port = args.port
+    display = args.display
+    save_path = args.save_file
 
-    cv2.imshow("FFmpeg UDP Stream", frame)
+    cap = cv2.VideoCapture(
+    f"udp://0.0.0.0:{port}?overrun_nonfatal=1&fifo_size=50000000",
+    cv2.CAP_FFMPEG
+    )
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if not cap.isOpened():
+        print("Failed to open video stream")
         cleanup()
+    
+    if save_path:
+        if not save_path.endswith('.avi'):
+            save_path += '.avi'
+
+        w = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        h = round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        if fps <= 1:
+            fps = 20
+        fourcc = cv2.VideoWriter.fourcc(*'MJPG')
+        
+        print(f"Opening video writer ({w}x{h} @ {fps} FPS) save to: {save_path}")
+        writer = cv2.VideoWriter(save_path, fourcc, fps, (w, h))
+
+        if not writer.isOpened():
+            print('Failed to open video writer')
+            cleanup()
+
+    signal.signal(signal.SIGINT, cleanup)
+    signal.signal(signal.SIGTERM, cleanup)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            cv2.waitKey(10)
+            continue
+        
+        if display:
+            cv2.imshow("FFmpeg UDP Stream", frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                cleanup()
+        
+        if writer is not None:
+            writer.write(frame)
+
+if __name__ == "__main__":
+    main()
 
 """
 import subprocess
